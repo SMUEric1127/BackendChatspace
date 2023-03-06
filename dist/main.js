dc1a544f687f9aaf68b088d9560606f474ab9100
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2023-03-05 21:14:09 -0600

    change port

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(4001);
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
