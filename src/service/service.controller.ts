4b9f36f477d705dd88f2276b9c966ee7b1e5114c
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2023-03-11 01:49:25 -0600

    Add token limit

--- src/service/service.controller.ts
+++ src/service/service.controller.ts
@@ -38,7 +38,7 @@ export class ServiceController {
         var response = openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             max_tokens: Number(process.env.MAX_TOKENS),
-            messages: [{ role: "user", content: prompt}],
+            messages: [{ role: "user", content: "Answer under 128 tokens\n " + prompt}],
         });
         response.then(async (response) => {
             // Completed, print out the response
